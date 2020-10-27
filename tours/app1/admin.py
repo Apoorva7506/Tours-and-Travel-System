b7771a46c300f1b3b29fd3e45e7395522bb76871
DeepNanda <deepnanda30@gmail.com>
2020-10-27 13:23:45 +0530

    models added

--- tours/app1/admin.py
+++ tours/app1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(CustomerNumber)
+admin.site.register(Destination)
+admin.site.register(PopularSpots)
+admin.site.register(Hotel)
+admin.site.register(Luxury)
--- tours/app1/models.py
+++ tours/app1/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    c_id=models.AutoField(primary_key=True)
+    fname=models.CharField(max_length=255)
+    mname=models.CharField(max_length=255)
+    lname=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    age=models.IntegerField()
+    houseno=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    state=models.CharField(max_length=255)
+    def __str__(self):
+        return self.fname
+    def customer_number():
+        return self.customernumber_set_values_list('cnumber',flat=True)
+
+
+class CustomerNumber(model.Model):
+    cid=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    cnumber=models.IntegerField(max_length=10)
+
+
+class Destination(models.Model):
+    d_id=models.AutoField(primary_key=True)
+    dname=models.CharField(max_length=255)
+    dstate=models.CharField(max_length=255)
+    def __str__(self):
+        return self.dname
+    def popular_spots():
+        return self.popularspots_set_values_list('pname',flat=True)
+
+
+class PopularSpots(models.Model):
+    did=models.ForeignKey(Destination,on_delete=models.CASCADE)
+    pname=models.CharField(max_length=255)
+
+
+class Hotel(models.Model):
+    h_id=models.AutoField(primary_key=True)
+    tier=models.IntegerField(max_length=1)
+    hname=models.CharField(max_length=255)
+    street=models.CharField(max_length=255)
+    locality=models.CharField(max_length=255)
+    d_id=models.ForeignKey(Destination,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.hname
+    def luxuries():
+        return self.luxury_set_values_list('name',flat=True)
+
+
+class Luxury(models.Model):
+    hid= models.ForeignKey(Destination,on_delete=models.CASCADE)
+    name= models.CharField(max_length=255)
