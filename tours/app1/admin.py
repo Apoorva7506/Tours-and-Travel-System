272591b58dfab4dbebf45925b5c4532fa64da50c
DeepNanda <deepnanda30@gmail.com>
2020-11-02 16:11:19 +0530

    custom user model

--- tours/app1/admin.py
+++ tours/app1/admin.py
@@ -1,5 +1,94 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
+from django.conf.urls import url
+from django import forms
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.urls import path, include
+from django.http import HttpResponse, HttpResponseRedirect
+from datetime import datetime
+
+class AddUserForm(forms.ModelForm):
+    """
+    New User Form. Requires password confirmation.
+    """
+    password1 = forms.CharField(
+        label='Password', widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Confirm password', widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'username', 'fname','mname','lname', 'phone' , 'age', 'houseno','city','state' 'password'
+        )
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UpdateUserForm(forms.ModelForm):
+    """
+    Update User Form. Doesn't allow changing password in the Admin.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email', 'password','username', 'fname','mname','lname', 'phone' , 'age', 'houseno','city','state'
+        )
+
+    def clean_password(self):
+        # Password can't be changed in the admin
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UpdateUserForm
+    add_form = AddUserForm
+
+    list_display = ('email',  'phone',
+                    'is_staff', 'username', 'fname','mname','lname', 'phone' , 'age', 'houseno','city','state')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ['name', 'username', 'phone', 'username', 'fname','mname','lname', 'phone' , 'age', 'houseno','city','state']}),
+
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',  'phone', 'username', 'password1',
+                    'password2', 'fname','mname','lname', 'phone' , 'age', 'houseno','city','state'
+                )
+            }
+        ),
+    )
+    search_fields = ('email', 'fname')
+    ordering = ('email', 'username', 'fname')
+    filter_horizontal = ()
+
+
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(CustomerNumber)
--- tours/app1/models.py
+++ tours/app1/models.py
@@ -1,6 +1,114 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from datetime import datetime
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(
+            self, email,username,fname,mname,lname, phone,age, houseno,city,state, password=None, commit=True):
+        if not email:
+            raise ValueError(('Users must have an email address'))
+        if not username:
+            raise ValueError(('Users must have a username'))
+        if not fname:
+            raise ValueError(('Users must have a first name'))
+        if not mname:
+            raise ValueError(('Users must have a middle name'))
+        if not nname:
+            raise ValueError(('Users must have a last name'))
+        if not phone:
+            raise ValueError(('Users must have a phone number'))
+        if not age:
+            raise ValueError(('Users must have an age'))
+        if not houseno:
+            raise ValueError(('Users must have a houseno'))
+        if not city:
+            raise ValueError(('Users must have a city'))    
+        if not state:
+            raise ValueError(('Users must have a state'))
+
+        user = self.model(
+            email=self.normalize_email(email),
+            fname=fname,
+            mname=mname,
+            lname=lname,
+            username=username,
+            age=age,
+            houseno=houseno,
+            city=city,
+            state=state,
+            phone=phone,
+        )
+
+        user.set_password(password)
+        if commit:
+            user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, fname,mname,lname, phone, password , age, houseno,city,state):
+        user = self.create_user(
+            email,
+            password=password,
+            fname=fname,
+            mname=mname,
+            lname=lname,
+            username=username,
+            age=age,
+            houseno=houseno,
+            city=city,
+            state=state,
+            phone=phone,
+            commit=False,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name=('email address'), max_length=255, unique=True)
+    username = models.CharField(('username'), max_length=100, blank=True, unique=True)
+    phone = models.CharField(max_length=10)
+    fname=models.CharField(('fname'),max_length=255)
+    mname=models.CharField(('mname'),max_length=255)
+    lname=models.CharField(('lname'),max_length=255)
+    age=models.IntegerField()
+    houseno=models.CharField(('houseno'),max_length=255)
+    city=models.CharField(('city'),max_length=255)
+    state=models.CharField(('state'),max_length=255)
+    is_active = models.BooleanField(
+        ('active'),
+        default=True,
+        help_text=(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
+    is_staff = models.BooleanField(
+        ('staff status'),
+        default=False,
+        help_text=(
+            'Designates whether the user can log into this admin site.'
+        ),
+    )
+
+    # is_superuser field provided by PermissionsMixin
+    # groups field provided by PermissionsMixin
+    # user_permissions field provided by PermissionsMixin
+
+    date_joined = models.DateTimeField(
+        ('date joined'), default=datetime.now
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'fname','mname','lname', 'phone' , 'age', 'houseno','city','state']
+
+    def __str__(self):
+        return(self.username)
+
 class Customer(models.Model):
     
     fname=models.CharField(max_length=255)
@@ -17,9 +125,9 @@ class Customer(models.Model):
         return self.customernumber_set_values_list('cnumber',flat=True)
 
 
-class CustomerNumber(model.Model):
+class CustomerNumber(models.Model):
     customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
-    cnumber=models.IntegerField(max_length=10)
+    cnumber=models.IntegerField()
 
 
 class Destination(models.Model):
@@ -37,7 +145,7 @@ class PopularSpots(models.Model):
 
 
 class Hotel(models.Model):
-    tier=models.IntegerField(max_length=1)
+    tier=models.IntegerField()
     hname=models.CharField(max_length=255)
     street=models.CharField(max_length=255)
     locality=models.CharField(max_length=255)
